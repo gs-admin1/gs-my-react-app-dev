bd068c1b57eb8d5745d138dc5310ef470bf80b13
HashimFitness <hashimfitness3@gmail.com>
2024-10-30 16:25:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-# Entry point for the React app
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AuthFlow from './AuthFlow';  // Import the main component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthFlow />  // Render the AuthFlow component as the app's entry point
+  </React.StrictMode>,
+  document.getElementById('root')
+);
